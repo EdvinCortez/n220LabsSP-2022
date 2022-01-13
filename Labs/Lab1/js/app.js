2f8e0d04e0fa5e1c846f55ed49416ef2b30d9dcd
EdvinCortez <71794708+EdvinCortez@users.noreply.github.com>
2022-01-13 15:57:05 -0500

    updating app.js

--- Labs/Lab1/js/app.js
+++ Labs/Lab1/js/app.js
@@ -5,7 +5,7 @@ function setup(){
 }//end setup
 
 function draw(){
-    //background(40); //darkens backround so you only have one circle, different under setup func
+    //background(40); //darkens backround so you only have one circle, different under setup function
     circle(mouseX, mouseY, frameCount/10); //(x,y,r)
 
 }//end draw
