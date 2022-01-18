01b83ccff8f171af18c7c85b3cfee0fea0229801
Edvin <edvin73c@hotmail.com>
2022-01-18 18:48:44 -0500

    In class, small changes

--- Labs/Lab1/js/app.js
+++ Labs/Lab1/js/app.js
@@ -1,11 +1,18 @@
+//Edvin Cortez
+//1/20/2022
+//n220
+//Comment the algorithm in the code
+
 function setup(){
     createCanvas(800, 600);
     background(40);
+    circle(150, 50, 30);
+    circle(300, 50, 30);
 
 }//end setup
 
 function draw(){
     //background(40); //darkens backround so you only have one circle, different under setup function
-    circle(mouseX, mouseY, frameCount/10); //(x,y,r)
+    //circle(mouseX, mouseY, frameCount/10); //(x,y,r)
 
 }//end draw
